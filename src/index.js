5aeb340d9369b3b7406747fe8781701c805230df
OlesiaTsat <112757674+OlesiaTsat@users.noreply.github.com>
2022-09-18 15:29:23 +0300

    added html dato to Js

--- src/index.js
+++ src/index.js
@@ -21,6 +21,32 @@ function formatDate(timestampe) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecasr() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/04d@2x.png"
+        alt=""
+        width="42px"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function dislayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -84,3 +110,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
+displayForecasr();
