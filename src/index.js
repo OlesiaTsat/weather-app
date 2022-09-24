100db5a1db0c5f05535a5a297d4dd96ab3014fe2
OlesiaTsat <112757674+OlesiaTsat@users.noreply.github.com>
2022-09-25 00:12:09 +0300

    fixed problem with time

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function formatDate(timestampe) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDate(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
